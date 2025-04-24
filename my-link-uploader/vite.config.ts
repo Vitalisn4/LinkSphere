80046c405a9342e7cdddaaa9ac800bc12cffd75d
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-04-24 14:57:21 +0100

    refactor(file): added dependencies

--- my-link-uploader/vite.config.ts
+++ my-link-uploader/vite.config.ts
@@ -4,4 +4,20 @@ import react from "@vitejs/plugin-react-swc"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: "jsx",
+    include: [
+      // Add visibility to the files you want to use JSX syntax in
+      // Transform .js files to process JSX
+      "src/**/*.js",
+    ],
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
