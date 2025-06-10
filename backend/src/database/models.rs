f1e9fc9ece5fb50c319fe751d8969c9bbaea4bf3
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-06-10 09:26:03 +0100

    feat(models): update Link model to use Uuid for ID and user identification, add click_count field for tracking link interactions

--- backend/src/database/models.rs
+++ backend/src/database/models.rs
@@ -1,22 +1,25 @@
 use serde::{Deserialize, Serialize};
 use chrono::{DateTime, Utc};
 use utoipa::ToSchema;
+use uuid::Uuid;
 
 /// Represents a link in the system
 #[derive(Debug, Serialize, Deserialize, ToSchema)]
 pub struct Link {
     /// Unique identifier for the link
-    pub id: i32,
+    pub id: Uuid,
     /// The URL of the link
     pub url: String,
     /// Title of the link
     pub title: String,
     /// Description of the link
     pub description: String,
-    /// Username of the link creator
-    pub username: String,
+    /// ID of the user who created the link
+    pub user_id: Uuid,
+    /// Number of times the link has been clicked
+    pub click_count: i32,
     /// When the link was created
     pub created_at: DateTime<Utc>,
     /// When the link was last updated
-    pub updated_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>
 } 
