e8da91f3d4c0ae9f8d1dae1914af0fd63b17bc43
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-06-14 19:37:04 +0100

    refactor(LoginForm): update import path for useAuth to hooks for better organization

--- my-link-uploader/src/components/auth/LoginForm.tsx
+++ my-link-uploader/src/components/auth/LoginForm.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { Mail, Lock, LogIn, ArrowRight } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../../contexts/AuthContext";
+import { useAuth } from "../../hooks/useAuth";
 
 export default function LoginForm() {
   const [email, setEmail] = useState("");
