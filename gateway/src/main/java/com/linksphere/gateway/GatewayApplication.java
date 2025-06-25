dabf7d3fb477a4711d78e3ee8b772d71dc0b00e6
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-06-25 23:38:51 +0100

    Remove unused ApplicationContext and Play class instantiation from main method

--- gateway/src/main/java/com/linksphere/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/linksphere/gateway/GatewayApplication.java
@@ -2,15 +2,12 @@ package com.linksphere.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(GatewayApplication.class, args);
-		Play obj = context.getBean(Play.class);
-		obj.greet("Nkwenti");
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
