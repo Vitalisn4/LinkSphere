95066aa3b7df7477dd8d801f68320f3720db6e5a
JOELNATHAN544 <wankojoelnathan@gmail.com>
2025-07-21 11:53:38 +0100

    refactor: wrap Layout component in SearchProvider to enable search context for improved user experience

--- my-link-uploader/src/components/Layout.tsx
+++ my-link-uploader/src/components/Layout.tsx
@@ -6,6 +6,7 @@ import { useAuth } from "../hooks/useAuth"
 import Sidebar from "./Sidebar"
 import Header from "./Header" // We will create this new component
 import { useLocation } from "react-router-dom"
+import { SearchProvider } from "../contexts/search";
 
 interface LayoutProps {
   children: ReactNode
@@ -33,22 +34,24 @@ export default function Layout({ children }: LayoutProps) {
   }
 
   return (
-    <div className="min-h-screen flex bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Header />
-        <main className="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 py-8">
-          <motion.div
-            key={location.pathname}
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -20 }}
-            transition={{ duration: 0.3 }}
-          >
-            {children}
-          </motion.div>
-        </main>
+    <SearchProvider>
+      <div className="min-h-screen flex bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white">
+        <Sidebar />
+        <div className="flex-1 flex flex-col">
+          <Header />
+          <main className="flex-1 w-full max-w-7xl mx-auto px-4 sm:px-6 py-8">
+            <motion.div
+              key={location.pathname}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.3 }}
+            >
+              {children}
+            </motion.div>
+          </main>
+        </div>
       </div>
-    </div>
+    </SearchProvider>
   )
 }
